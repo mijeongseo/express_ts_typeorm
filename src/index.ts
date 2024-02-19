f7219dff66e61a52b4db0f1fef2fdfad69851566
mijeongseo <mimiseo@naraspace.com>
2024-02-19 12:53:02 +0900

    feat : db 초기화 후 express server 실행

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,43 @@
 import express, { Express, Request, Response } from 'express';
 import routes from '@routes/index';
-import dataSources from './dataSource';
-
-Promise.all(
-    dataSources.map(async (dataSource, idx) => {
-        await dataSource
-            .initialize()
-            .then(() => {
-                console.log(`Data Source ${idx + 1} has been initialized!`);
-            })
-            .catch((err) => {
-                console.error(`Error during Data Source ${idx + 1} initialization:`, err);
-            });
-    })
-);
+import { initializeDatabase } from './dataSource';
 
 const app: Express = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-app.set('port', process.env.PORT || 3000);
-app.get('/', (req: Request, res: Response) => {
-    res.send(`${process.env.NODE_ENV || 'development'}-server is running`);
-});
+/* 
+미들웨어 설정
+*/
+{
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+}
 
-app.use('/', routes);
-app.listen(app.get('port'), () => {
-    console.log(app.get('port'), '번 포트에 서버 실행');
-});
+/* 
+라우팅 설정
+*/
+{
+    app.get('/', (req: Request, res: Response) => {
+        res.send(`${process.env.NODE_ENV || 'development'}-server is running`);
+    });
+    app.get('/health-check', (req, res) => {
+        const data = {
+            uptime: process.uptime(),
+            message: 'Ok',
+            date: new Date(),
+        };
+        res.status(200).send(data);
+    });
+    app.use('/', routes);
+}
+
+/* 
+express server 실행
+*/
+{
+    app.set('port', process.env.PORT || 3000);
+    initializeDatabase().then(() =>
+        app.listen(app.get('port'), () => {
+            console.log(app.get('port'), '번 포트에 서버 실행');
+        })
+    );
+}
